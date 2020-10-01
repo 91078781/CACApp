faadf3b601b7cce20e37fca9b54e2dde8d21314d
Jaqueline Gomez <91078781@mvusd.net>
2020-10-01 09:06:39 -0700

    Navigation out of AboutTaraCalico screen

--- app/src/main/java/com/example/cacapp/AboutTaraCalico.java
+++ app/src/main/java/com/example/cacapp/AboutTaraCalico.java
@@ -3,6 +3,7 @@ package com.example.cacapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutTaraCalico extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AboutTaraCalico extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_tara_calico);
+    }
+    public void onClickReturnMissingPersonScnBtn_TaraC(View view)
+    {
+        finish();
     }
 }
