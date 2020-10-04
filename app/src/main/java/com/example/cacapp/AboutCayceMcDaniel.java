576f7b3bae86238ece02b8b4c98d4856c227aacd
Jaqueline Gomez <91078781@mvusd.net>
2020-10-04 16:57:32 -0700

    Navigation out of AboutCayceMcDaniel screen

--- app/src/main/java/com/example/cacapp/AboutCayceMcDaniel.java
+++ app/src/main/java/com/example/cacapp/AboutCayceMcDaniel.java
@@ -3,6 +3,7 @@ package com.example.cacapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutCayceMcDaniel extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class AboutCayceMcDaniel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_cayce_mc_daniel);
+    }
+
+    public void onClickReturnMissingPersonScn_cayce_mcdaniel(View view)
+    {
+        finish();
     }
 }
