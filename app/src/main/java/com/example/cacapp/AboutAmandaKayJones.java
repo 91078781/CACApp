7bc61015fd3bea04d380c4d791b32c43989d4e9f
Jaqueline Gomez <91078781@mvusd.net>
2020-10-04 13:04:42 -0700

    Navigation out of AboutAmandaKayJones screen

--- app/src/main/java/com/example/cacapp/AboutAmandaKayJones.java
+++ app/src/main/java/com/example/cacapp/AboutAmandaKayJones.java
@@ -3,6 +3,7 @@ package com.example.cacapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutAmandaKayJones extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class AboutAmandaKayJones extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_amanda_kay_jones);
     }
+    public void onClickReturnToMissingPeopleScn_AmandaJones(View view)
+    {
+        finish();
+    }
+
 }
