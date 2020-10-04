20a4704ef248c54109b9950f911c67e8dbca35b9
Jaqueline Gomez <91078781@mvusd.net>
2020-10-04 13:14:54 -0700

    Navigation out of AboutBreasiaTerrell screen

--- app/src/main/java/com/example/cacapp/AboutBreasiaTerrell.java
+++ app/src/main/java/com/example/cacapp/AboutBreasiaTerrell.java
@@ -3,6 +3,7 @@ package com.example.cacapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutBreasiaTerrell extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class AboutBreasiaTerrell extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_breasia_terrell);
+    }
+
+    public void onClickReturnToMissingPeopleScn_BreasiaTerrell(View view)
+    {
+        finish();
     }
 }
