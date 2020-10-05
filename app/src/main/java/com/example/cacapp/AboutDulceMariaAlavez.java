6cb1c12930c60625094a7a238840d1646bfa6470
Jaqueline Gomez <91078781@mvusd.net>
2020-10-04 17:04:42 -0700

    Navigation out of Dulce Maria Alavez screen

--- app/src/main/java/com/example/cacapp/AboutDulceMariaAlavez.java
+++ app/src/main/java/com/example/cacapp/AboutDulceMariaAlavez.java
@@ -3,6 +3,7 @@ package com.example.cacapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutDulceMariaAlavez extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class AboutDulceMariaAlavez extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_dulce_maria_alavez);
+    }
+
+    public void onClickReturnToMissingPeopleScn_dulceMariaAlavez(View view)
+    {
+        finish();
     }
 }
