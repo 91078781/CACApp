9d7c44e7c04f5c975830f4c1ffaebad2581fb63a
Jaqueline Gomez <91078781@mvusd.net>
2020-10-04 17:20:53 -0700

    Navigation out of AboutCrystalRogers screen added

--- app/src/main/java/com/example/cacapp/AboutCrystalRogers.java
+++ app/src/main/java/com/example/cacapp/AboutCrystalRogers.java
@@ -3,6 +3,7 @@ package com.example.cacapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutCrystalRogers extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class AboutCrystalRogers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_crystal_rogers);
+    }
+
+    public void onClickReturnToMissingPeopleScn_crystalRogers(View view)
+    {
+        finish();
     }
 }
