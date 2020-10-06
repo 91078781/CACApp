c48c5e326aae0fd376c61f732a6458c411ff7246
Jaqueline Gomez <91078781@mvusd.net>
2020-10-05 20:39:15 -0700

    Created MissingPeople objects in AboutTaraCalico screen

--- app/src/main/java/com/example/cacapp/AboutTaraCalico.java
+++ app/src/main/java/com/example/cacapp/AboutTaraCalico.java
@@ -6,64 +6,50 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AboutTaraCalico extends AppCompatActivity
 {
-
-    private int age;
-    private String name;
-    private String DOB;
-    private String lastSeenDate;
-    private String locationLastSeen;
-    private boolean hasReward;
-    private String scarsAndMarks;
-    private String contactNum;
-    private String sex;
-    private String hair;
-    private String weight;
-    private String height;
-    private String race;
-    private String eyeColor;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_tara_calico);
+        MissingPeople tara_calico = new MissingPeople();
+        tara_calico.setAge(49);
+        tara_calico.setName("Tara Calico");
+        tara_calico.setLastSeenDate("September 20,1988");
+        tara_calico.setDOB("Feburary 28, 2969");
+        tara_calico.setLocationLastSeen("Belen, New Mexico");
+        tara_calico.setHasReward(false);
+        tara_calico.setScarsAndMarks("Large Scar on right shoulder and brown birthmark in her leg");
+        tara_calico.setContactNum("N/A");
+        tara_calico.setSex("Female");
+        tara_calico.setHair("brown");
+        tara_calico.setEyeColor("Hazel");
+        tara_calico.setHeight("5'5\"");
+        tara_calico.setWeight("115- 120lbs");
+        tara_calico.setRace("White");
 
-        setAge(49);
-        setName("Tara Calico");
-        setLastSeenDate("September 20,1988");
-        setDOB("Feburary 28, 2969");
-        setLocationLastSeen("Belen, New Mexico");
-        setHasReward(false);
-        setScarsAndMarks("Large Scar on right shoulder and brown birthmark in her leg");
-        setContactNum("N/A");
-        setSex("Female");
-        setHair("brown");
-        setEyeColor("Hazel");
-        setHeight("5\'5\"");
-        setWeight("115- 120lbs");
-        setRace("White");
+        Log.i("TESTING", "--------------------------"+ tara_calico.toString()+"----------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getAge()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getName()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getDOB()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getLastSeenDate()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getLocationLastSeen()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getHasReward()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getScarsAndMarks()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getContactNum()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getSex()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getHair()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getEyeColor()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getHeight()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getWeight()+"--------------------------");
+        Log.i("TESTING", "--------------------------"+ tara_calico.getRace()+"--------------------------");
 
-        Log.i("info", "--------------------------"+ toString()+"----------------------------");
-
-        Log.i("info", "--------------------------"+ getAge()+"--------------------------");
-        Log.i("info", "--------------------------"+ getName()+"--------------------------");
-        Log.i("info", "--------------------------"+ getDOB()+"--------------------------");
-        Log.i("info", "--------------------------"+ getLastSeenDate()+"--------------------------");
-        Log.i("info", "--------------------------"+ getLocationLastSeen()+"--------------------------");
-        Log.i("info", "--------------------------"+ getHasReward()+"--------------------------");
-        Log.i("info", "--------------------------"+ getScarsAndMarks()+"--------------------------");
-        Log.i("info", "--------------------------"+ getContactNum()+"--------------------------");
-        Log.i("info", "--------------------------"+ getSex()+"--------------------------");
-        Log.i("info", "--------------------------"+ getHair()+"--------------------------");
-        Log.i("info", "--------------------------"+ getEyeColor()+"--------------------------");
-        Log.i("info", "--------------------------"+ getHeight()+"--------------------------");
-        Log.i("info", "--------------------------"+ getWeight()+"--------------------------");
-        Log.i("info", "--------------------------"+ getRace()+"--------------------------");
-
+        Toast toast = Toast.makeText(getApplicationContext(),"--------------------------"+ tara_calico.toString()+"----------------------------",Toast.LENGTH_LONG);
+        toast.show();
     }
 
     public void onClickReturnMissingPersonScnBtn_TaraC(View view)
@@ -71,147 +57,5 @@ public class AboutTaraCalico extends AppCompatActivity
         finish();
     }
 
-    /*Getters*/
-    public int getAge() {
-        return age;
-    }//end method getAge()
 
-    public String getName() {
-        return name;
-    }//end getName()
-
-    public String getDOB() {
-        return DOB;
-    }//end getDOB
-
-    public String getLastSeenDate() {
-        return lastSeenDate;
-    }// end getLastSeenDate()
-
-    public String getLocationLastSeen() {
-        return locationLastSeen;
-    }//end getLocationLastSeen()
-
-    public boolean getHasReward()
-    {
-        return hasReward;
-    }
-    public String getScarsAndMarks() {
-        return scarsAndMarks;
-    }//end getScarsAndMarks()
-
-    public String getContactNum() {
-        return contactNum;
-    }//end getContactNum()
-
-    public String getSex() {
-        return sex;
-    }//end getSex()
-
-    public String getHeight() {
-        return height;
-    }//end getHeight()
-
-    public String getWeight() {
-        return weight;
-    }//end getWeight()
-
-    public String getRace() {
-        return race;
-    }//end getRace()
-
-    public String getEyeColor() {
-        return eyeColor;
-    }// end getEyeColor()
-
-    public String getHair() {
-        return hair;
-    }//end getHair()
-
-    /*Setters*/
-    public void setAge(int age) {
-        this.age = age;
-    }//end setAge()
-
-    public void setName(String name) {
-        this.name = name;
-    }//end setName()
-
-    public void setDOB(String DOB) {
-        this.DOB = DOB;
-    }//end setDOB()
-
-    public void setLastSeenDate(String lastSeenDate) {
-        this.lastSeenDate = lastSeenDate;
-    }// end setLastSeenDate()
-
-    public void setLocationLastSeen(String locationLastSeen) {
-        this.locationLastSeen = locationLastSeen;
-    }//end setLocationLastSeen()
-
-    public void setHasReward(boolean hasReward) {
-        this.hasReward = hasReward;
-    }//end setHasReward()
-
-    public void setScarsAndMarks(String scarsAndMarks) {
-        this.scarsAndMarks = scarsAndMarks;
-    }//end setScarsAndMarks()
-
-    public void setContactNum(String contactNum) {
-        this.contactNum = contactNum;
-    }//end setContactNum()
-
-    public void setRace(String race) {
-        this.race = race;
-    }// end setRace()
-
-    public void setEyeColor(String eyeColor) {
-        this.eyeColor = eyeColor;
-    }//end setEyeColor()
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }//end setSex()
-
-    public void setHair(String hair) {
-        this.hair = hair;
-    }//end setHair()
-
-    public void setHeight(String height) {
-        this.height = height;
-    }//end setHeight()
-
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }//end setWeight()
-
-
-//    /*Edit TextViews*/
-//
-//    public void editLastSeen()
-//    {
-//        TextView currentTV = (TextView) findViewById(R.id.lastSeen_taraCalico);
-//        String newTv = currentTV.getText().toString()+ lastSeenDate;
-//    }
-
-
-    @Override
-    public String toString() {
-        return "AboutTaraCalico{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", DOB='" + DOB + '\'' +
-                ", lastSeenDate='" + lastSeenDate + '\'' +
-                ", locationLastSeen='" + locationLastSeen + '\'' +
-                ", hasReward=" + hasReward +
-                ", scarsAndMarks='" + scarsAndMarks + '\'' +
-                ", contactNum='" + contactNum + '\'' +
-                ", sex='" + sex + '\'' +
-                ", hair='" + hair + '\'' +
-                ", weight='" + weight + '\'' +
-                ", height='" + height + '\'' +
-                ", race='" + race + '\'' +
-                ", eyeColor='" + eyeColor + '\'' +
-                '}';
-    }
 }
--- app/src/main/java/com/example/cacapp/MissingPeople.java
+++ app/src/main/java/com/example/cacapp/MissingPeople.java
@@ -2,6 +2,7 @@ package com.example.cacapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +13,170 @@ import androidx.fragment.app.Fragment;
 
 public class MissingPeople extends Fragment
 {
+    private int age;
+    private String name;
+    private String DOB;
+    private String lastSeenDate;
+    private String locationLastSeen;
+    private boolean hasReward;
+    private String scarsAndMarks;
+    private String contactNum;
+    private String sex;
+    private String hair;
+    private String weight;
+    private String height;
+    private String race;
+    private String eyeColor;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.missing_people_layout, container, false);
     }
+
+
+    /*Getters*/
+    public int getAge() {
+        return age;
+    }//end method getAge()
+
+    public String getName() {
+        return name;
+    }//end getName()
+
+    public String getDOB() {
+        return DOB;
+    }//end getDOB
+
+    public String getLastSeenDate() {
+        return lastSeenDate;
+    }// end getLastSeenDate()
+
+    public String getLocationLastSeen() {
+        return locationLastSeen;
+    }//end getLocationLastSeen()
+
+    public boolean getHasReward()
+    {
+        return hasReward;
+    }
+    public String getScarsAndMarks() {
+        return scarsAndMarks;
+    }//end getScarsAndMarks()
+
+    public String getContactNum() {
+        return contactNum;
+    }//end getContactNum()
+
+    public String getSex() {
+        return sex;
+    }//end getSex()
+
+    public String getHeight() {
+        return height;
+    }//end getHeight()
+
+    public String getWeight() {
+        return weight;
+    }//end getWeight()
+
+    public String getRace() {
+        return race;
+    }//end getRace()
+
+    public String getEyeColor() {
+        return eyeColor;
+    }// end getEyeColor()
+
+    public String getHair() {
+        return hair;
+    }//end getHair()
+
+    /*Setters*/
+    public void setAge(int age) {
+        this.age = age;
+    }//end setAge()
+
+    public void setName(String name) {
+        this.name = name;
+    }//end setName()
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }//end setDOB()
+
+    public void setLastSeenDate(String lastSeenDate) {
+        this.lastSeenDate = lastSeenDate;
+    }// end setLastSeenDate()
+
+    public void setLocationLastSeen(String locationLastSeen) {
+        this.locationLastSeen = locationLastSeen;
+    }//end setLocationLastSeen()
+
+    public void setHasReward(boolean hasReward) {
+        this.hasReward = hasReward;
+    }//end setHasReward()
+
+    public void setScarsAndMarks(String scarsAndMarks) {
+        this.scarsAndMarks = scarsAndMarks;
+    }//end setScarsAndMarks()
+
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }//end setContactNum()
+
+    public void setRace(String race) {
+        this.race = race;
+    }// end setRace()
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }//end setEyeColor()
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }//end setSex()
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }//end setHair()
+
+    public void setHeight(String height) {
+        this.height = height;
+    }//end setHeight()
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }//end setWeight()
+
+
+//    /*Edit TextViews*/
+//
+//    public void editLastSeen()
+//    {
+//        TextView currentTV = (TextView) findViewById(R.id.lastSeen_taraCalico);
+//        String newTv = currentTV.getText().toString()+ lastSeenDate;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "age=" + age +
+                ", name='" + name + '\'' +
+                ", DOB='" + DOB + '\'' +
+                ", lastSeenDate='" + lastSeenDate + '\'' +
+                ", locationLastSeen='" + locationLastSeen + '\'' +
+                ", hasReward=" + hasReward +
+                ", scarsAndMarks='" + scarsAndMarks + '\'' +
+                ", contactNum='" + contactNum + '\'' +
+                ", sex='" + sex + '\'' +
+                ", hair='" + hair + '\'' +
+                ", weight='" + weight + '\'' +
+                ", height='" + height + '\'' +
+                ", race='" + race + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                '}';
+    }
+
 
 }
